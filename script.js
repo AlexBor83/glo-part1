d33fcb9a0ceb583aa9b1a1ed6909158f69535638
AlexBor83 <rabota_for_alex@mail.ru>
2022-07-30 22:54:34 +0400

    2 задание

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 console.log('привет мир');
 
-let title, 
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+const title = 'Hello GLO';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 10;
+const rollback = 50;
+const fullPrice = 100000;
+const adaptive = true;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice + ' $');
 
-alert('hello');
+console.log(screens.toLowerCase().split(''));
+
+console.log(fullPrice * (rollback/100));
+
