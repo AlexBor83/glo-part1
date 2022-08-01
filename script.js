73d272cfab1cb1d6aca1a5d1041a7a9460f4dac2
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-01 22:29:36 +0400

    готово 3 задание

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
+'use strict';
+
 console.log('привет мир');
 
-const title = 'Hello GLO';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 10;
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', ('Простые, Сложные, Интерактивные'));
+const screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
 const rollback = 50;
-const fullPrice = 100000;
-const adaptive = true;
 
+const adaptiveNeed = prompt('Нужен ли адаптив на сайте?');
+let adaptive = false;
+if (adaptiveNeed == 'да') {
+    adaptive = true;
+}
 
+console.log(adaptiveNeed);
+console.log(adaptive);
+
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
+
+console.log(servicePercentPrice);
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 console.log(screenPrice + ' $');
-
-console.log(screens.toLowerCase().split(''));
-
+console.log(screens.toLowerCase().split(' '));
 console.log(fullPrice * (rollback/100));
 
+
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');    
+} else if (fullPrice < 30000 && fullPrice > 15000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice <= 15000 && fullPrice >= 0) {
+    console.log('Скидка не предусмотрена');    
+} else {
+    console.log('Что то пошло не так');
+}
